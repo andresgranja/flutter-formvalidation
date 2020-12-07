8aa10c0e7f348c33847aa3bdee775c3a15a10139
Andres Granja <andresgranja001@gmail.com>
2020-12-07 13:16:54 -0500

    code uploaded

---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+ 
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          crearFondo()
+        ],
+      ),
+    );
+  }
+
+  Widget crearFondo( ) {
+
+  }
+}
